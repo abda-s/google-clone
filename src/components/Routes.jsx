beb450f4ef04dc22bbbebf5c0006a755bc828c5c
abdullah salameh <3bda.as@gmail.com>
2022-04-26 15:06:18 +0300

    adding router components 📍

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Switch, Route, Redirect} from "react-loader-dom"
 
 export const Routes = () => {
   return (
-    <div>Routes</div>
+    <div className='p-4'>
+        <Switch>
+            <Route exact path="/">
+                <Redirect to="/search"  />
+            </Route>
+            <Route exact path={['/search', '/images', '/news', '/videos']}> 
+
+            </Route>
+        </Switch>  
+    </div>
   )
 }
 

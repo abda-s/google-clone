6ccf931b47a8570200b8a463c13e62e69453693d
abdullah salameh <3bda.as@gmail.com>
2022-04-26 07:36:34 +0300

    cleaning some code and adding tailwind 👽

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import React from 'react'
+import "./index.css"
 
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  );
-}
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
 
+    </div>
+  )
+}
 export default App;

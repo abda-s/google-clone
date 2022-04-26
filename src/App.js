2c83fb5b7fb57036723506dca29acb36ab7a9030
abdullah salameh <3bda.as@gmail.com>
2022-04-26 08:40:16 +0300

    Dark mode added 🌙

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, {useState} from 'react'
 import "./index.css"
 
-import  Navbar  from "./components/Navbar";
-import  Footer  from "./components/Footer";
-import  Routes  from "./components/Routes";
+import  {Navbar}  from "./components/Navbar";
+import  {Footer}  from "./components/Footer";
+import  {Routes}  from "./components/Routes";
 
 export const App = () => {
     const [darkTheme,setDarkTheme] = useState(false);
 
   return (
-    <div className={darkTheme ? "dark" : ""}>
-        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-            <Navbar />
-            <Routes />
-            <Footer />
-        </div>
+    <div className={darkTheme ? 'dark' : ''}>
+    <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+      <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+      <Routes />
+      <Footer />
     </div>
-  )
-}
+  </div>
+  );
+};
 export default App;

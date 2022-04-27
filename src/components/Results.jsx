a94667df5443a65effd9606b2f4ea5d256f6ac00
abdullah salameh <3bda.as@gmail.com>
2022-04-27 17:31:04 +0300

    importing ✨

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+
+import {useResultContext} from "../contexts/ResultContextProvider";
 
 export const Results = () => {
+
+  
+
   return (
     <div>
         Results

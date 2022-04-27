f4a9c33248283be5dc23a8bf810def918f9777ba
abdullah salameh <3bda.as@gmail.com>
2022-04-27 16:53:02 +0300

    adding fetch and dot env for the api key 🔑

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,3 +1,25 @@
 import React,{creactContext, useContext, useState} from "react";
 
 const ResultContext = creactContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+
+
+    const getResults = async (type) => {
+        setIsLoading(true);
+
+        const response = await fetch(`${baseUrl}${type}`, {
+            method:"GET",
+            headers:{
+                'X-User-Agent': 'desktop',
+                'X-Proxy-Location': 'EU',
+                'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+                'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
+              }
+        })
+    };
+}

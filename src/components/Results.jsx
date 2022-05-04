64dce6ee3315a53a76eaecba1306e116ad2fbc00
abdullah salameh <3bda.as@gmail.com>
2022-05-04 16:46:57 +0300

    image result added 👌

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -39,10 +39,13 @@ export const Results = () => {
       case "/images":
         return (
           <div className='flex flex-wrap justify-center items-center'>
-            {results?.image_results?.map(()=>(
-              <div>
-                
-              </div>
+            {results?.image_results?.map(({image, link:{href,title}},index)=>(
+              <a className='sm:p-3 p-5' href={href} key={index} target="_blank" rel="noreferrer">
+                <img src={image?.src} alt={title} loading="lazy" />
+                <p className='w-36 break-words text-sm mt-2'>
+
+                </p>
+              </a>
             ))}
             
           </div>
